959ea93b3832ef5570761a69d9eb4b35e04d6e18
Eyob bokru <78879907+eyobbokru@users.noreply.github.com>
2024-01-16 12:26:22 +0900

    add item complete

--- item/views.py
+++ item/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Item
 # Create your views here.
-
-
+from django.contrib.auth.decorators import login_required
+from .forms import NewItemForm
 def detail(request, pk):
 
     item = get_object_or_404(Item, pk=pk)
@@ -12,3 +12,22 @@ def detail(request, pk):
                       'item':item,
                       'related_items':related_items
                   })
+
+@login_required
+def new(request):
+    
+    if request.method =='POST':
+       form = NewItemForm(request.POST, request.FILES)
+
+       if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {'form':form,
+                                              'title':'New item'})
